a2453babe0d31a993867aa29326d7b79d01578bb
Basil Shubin <basil.shubin@gmail.com>
2021-11-26 15:08:45 +0700

    Formatting (black)

--- feincms_button/contents.py
+++ feincms_button/contents.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _, pgettext_lazy
-
+from django.utils.translation import gettext_lazy as _
+from django.utils.translation import pgettext_lazy
 from feincms._internal import ct_render_to_string
 
 from . import appsettings
@@ -11,20 +11,21 @@ class ButtonContent(models.Model):
     Pager item, to show a previous/next page.
     The pages are auto determined, but can be overwritten
     """
+
     ALIGN_CHOICES = (
-        ('', pgettext_lazy("align", "Inline")),
-        ('left', pgettext_lazy("align", "Left")),
-        ('center', pgettext_lazy("align", "Center")),
-        ('right', pgettext_lazy("align", "Right")),
-        ('block', pgettext_lazy("align", "Full Width")),
+        ("", pgettext_lazy("align", "Inline")),
+        ("left", pgettext_lazy("align", "Left")),
+        ("center", pgettext_lazy("align", "Center")),
+        ("right", pgettext_lazy("align", "Right")),
+        ("block", pgettext_lazy("align", "Full Width")),
     )
 
     title = models.CharField(_("title"), max_length=200)
     url = models.URLField(_("URL"))
 
     style = models.CharField(_("style"), max_length=50, choices=appsettings.FEINCMS_BUTTON_STYLES)
-    size = models.CharField(_("size"), blank=True, default='', max_length=10, choices=appsettings.FEINCMS_BUTTON_SIZES)
-    align = models.CharField(_("alignment"), blank=True, default='', max_length=50, choices=ALIGN_CHOICES)
+    size = models.CharField(_("size"), blank=True, default="", max_length=10, choices=appsettings.FEINCMS_BUTTON_SIZES)
+    align = models.CharField(_("alignment"), blank=True, default="", max_length=50, choices=ALIGN_CHOICES)
 
     class Meta:
         abstract = True
@@ -39,16 +40,16 @@ class ButtonContent(models.Model):
 
     @property
     def css_classes(self):
-        classes = ['btn', self.style, self.size or '']
+        classes = ["btn", self.style, self.size or ""]
         if self.align:
-            if self.align == 'left':
-                classes.append('pull-left')
-            elif self.align == 'right':
-                classes.append('pull-right')
-            elif self.align == 'block':
-                classes.append('btn-block')
+            if self.align == "left":
+                classes.append("pull-left")
+            elif self.align == "right":
+                classes.append("pull-right")
+            elif self.align == "block":
+                classes.append("btn-block")
 
-        return ' '.join(classes).rstrip().replace('  ', ' ')
+        return " ".join(classes).rstrip().replace("  ", " ")
 
     def render(self, **kwargs):
         return ct_render_to_string(
@@ -57,4 +58,3 @@ class ButtonContent(models.Model):
             request=kwargs.get("request"),
             context=kwargs.get("context"),
         )
-
--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import os
-from setuptools import setup, find_packages
+
+from setuptools import find_packages, setup
 
 
 def read(filename):
@@ -12,43 +13,37 @@ setup(
     name="feincms-button",
     version=__import__("feincms_button").__version__,
     license="Apache 2.0",
-
     install_requires=[
-        'FeinCMS>=1.15',
+        "FeinCMS>=1.15",
     ],
-
     description="Bootstrap 3 Button element for FeinCMS",
     long_description=read("README.rst"),
-
     author="Basil Shubin",
     author_email="basil.shubin@gmail.com",
-
     url="https://github.com/bashu/feincms-button/",
-    download_url='https://github.com/bashu/feincms-button/zipball/master',
-
-    packages=find_packages(exclude=('example*',)),
+    download_url="https://github.com/bashu/feincms-button/zipball/master",
+    packages=find_packages(exclude=("example*",)),
     include_package_data=True,
-
     zip_safe=False,
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Framework :: Django',
-        'Framework :: Django :: 1.10',
-        'Framework :: Django :: 1.11',
-        'Framework :: Django :: 2.0',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-    ]
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Framework :: Django",
+        "Framework :: Django :: 1.10",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
 )
