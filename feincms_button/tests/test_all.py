9f1b0ab7827f3270ade93c6c41153503175bca08
Basil Shubin <144783+bashu@users.noreply.github.com>
2026-08-14 12:57:10 +0600

    Add tests to reach 100% coverage on ButtonContent
    - Cover the left/right/block branches of css_classes (only "center"
      and unset were exercised before)
    - Cover __str__ and get_absolute_url()

--- feincms_button/tests/test_all.py
+++ feincms_button/tests/test_all.py
@@ -42,3 +42,76 @@ class ButtonContentTest(TestCase):
             '<a href="http://example.com" class="btn btn-default">TEST2</a>'
             "</p>",
         )
+
+    def test_align_left(self):
+        content = PageButtonContent.objects.create(
+            parent=self.page,
+            region="main",
+            url="http://example.com",
+            style="btn-default",
+            title="TEST3",
+            align="left",
+        )
+
+        html = str(content.render())
+
+        self.assertHTMLEqual(
+            html,
+            '<a href="http://example.com" class="btn btn-default pull-left">TEST3</a>',
+        )
+
+    def test_align_right(self):
+        content = PageButtonContent.objects.create(
+            parent=self.page,
+            region="main",
+            url="http://example.com",
+            style="btn-default",
+            title="TEST4",
+            align="right",
+        )
+
+        html = str(content.render())
+
+        self.assertHTMLEqual(
+            html,
+            '<a href="http://example.com" class="btn btn-default pull-right">TEST4</a>',
+        )
+
+    def test_align_block(self):
+        content = PageButtonContent.objects.create(
+            parent=self.page,
+            region="main",
+            url="http://example.com",
+            style="btn-default",
+            title="TEST5",
+            align="block",
+        )
+
+        html = str(content.render())
+
+        self.assertHTMLEqual(
+            html,
+            '<a href="http://example.com" class="btn btn-default btn-block">TEST5</a>',
+        )
+
+    def test_object(self):
+        content = PageButtonContent.objects.create(
+            parent=self.page,
+            region="main",
+            url="http://example.com",
+            style="btn-default",
+            title="TEST-STR",
+        )
+
+        self.assertEqual(str(content), "TEST-STR")
+
+    def test_get_absolute_url(self):
+        content = PageButtonContent.objects.create(
+            parent=self.page,
+            region="main",
+            url="http://example.com",
+            style="btn-default",
+            title="TEST-URL",
+        )
+
+        self.assertEqual(content.get_absolute_url(), "http://example.com")
